adee40c0741f211973cbc7c2e4089287d8e7abb4
Sen <telderasil@mail.ru>
2020-01-23 00:10:41 +0300

    Update OsuNono2Keys.ino

--- OsuNono2Keys.ino
+++ OsuNono2Keys.ino
@@ -1,14 +1,19 @@
 #include "Keyboard.h"
 
 const int profilesKey = 15;
+
 const int boardKey1 = 2;
 const int boardKey2 = 3;
+
 const int blueKey1 = 5;
 const int blueKey2 = 6;
 const int blueKey3 = 4;
 
-int boardKey1_PreviousState;
-int boardKey2_PreviousState;
+const int Led1 = 9;
+const int Led2 = 10;
+
+bool bkey1 = true;
+bool bkey2 = true;
 
 int currentProfile = 0;
 
@@ -19,56 +24,118 @@ void setup() {
   pinMode(blueKey1, INPUT_PULLUP);
   pinMode(blueKey2, INPUT_PULLUP);
   pinMode(blueKey3, INPUT_PULLUP);
+  pinMode(Led1, OUTPUT);
+  pinMode(Led2, OUTPUT);
   Keyboard.begin();
-  boardKey1_PreviousState = digitalRead(boardKey1);
-  boardKey2_PreviousState = digitalRead(boardKey2);
 }
-
 void loop() {
- int boardKey1_State = digitalRead(boardKey1);
- int boardKey2_State = digitalRead(boardKey2);
-
   if(digitalRead(profilesKey)==LOW){
+    digitalWrite(Led1, LOW);
+    digitalWrite(Led2, LOW);
     if(currentProfile+1>2){
       currentProfile=0;
+      bkey1 = true; 
+      bkey2 = true;
     }
     else{
       currentProfile++;
     }
     delay(300);
   }
- if(currentProfile==1){
-   if(boardKey1_PreviousState != boardKey1_State){
-    Keyboard.press('M');
+  
+  if(currentProfile==0){
+    
+    if(digitalRead(boardKey1)==LOW){
+      if(bkey1){
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('C');
+        delay(10);
+        Keyboard.release(KEY_LEFT_CTRL);
+        Keyboard.release('C');
+      }
+      bkey1 = false;
     }
-   else{
-    Keyboard.release('M');
+    else{
+      bkey1 = true;
+    }
+    
+    if(digitalRead(boardKey2)==LOW){
+      if(bkey2){
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('V');
+        //delay(10);
+        Keyboard.release(KEY_LEFT_CTRL);
+        Keyboard.release('V');
+      }
+      bkey2 = false;
+    }
+    else{
+      bkey2 = true;
+    }
+    
+    if(digitalRead(blueKey1)==LOW){
+      Keyboard.press(' ');
+    }
+    else{
+      Keyboard.release(' ');
+    }
+    
+    if(digitalRead(blueKey2)==LOW){
+      Keyboard.press('\n');
+    }
+    else{
+      Keyboard.release('\n');
+    }
+    
+    if(digitalRead(blueKey3)==LOW){
+      Keyboard.press(KEY_BACKSPACE);
+    }
+    else{
+      Keyboard.release(KEY_BACKSPACE);
+    }
+  }
+  else if(currentProfile==1){
+    
+    digitalWrite(Led1, HIGH);
+  
+    if(digitalRead(boardKey1)==LOW){
+      Keyboard.press('M');
+    }
+    else{
+      Keyboard.release('M');
     }
  
-  if(boardKey2_PreviousState != boardKey2_State){
-    Keyboard.press('N');
-  }
-  else{
-    Keyboard.release('N');
-  }
+    if(digitalRead(boardKey2)==LOW){
+      Keyboard.press('N');
+    }
+    else{
+      Keyboard.release('N');
+    }
+    
+    if(digitalRead(blueKey1)==LOW){
+      Keyboard.press(KEY_RIGHT_ALT);
+    }
+    else{
+      Keyboard.release(KEY_RIGHT_ALT);
+    }
+    
+    if(digitalRead(blueKey2)==LOW){
+      Keyboard.press(' ');
+    }
+    else{
+      Keyboard.release(' ');
+    }
   
-  if(digitalRead(blueKey1)==LOW){
-    Keyboard.press(KEY_RIGHT_ALT);
-    delay(150);
-    Keyboard.release(KEY_RIGHT_ALT);
-    delay(10);
+    if(digitalRead(blueKey3)==LOW){
+      Keyboard.press(KEY_ESC);
+    }
+    else{
+      Keyboard.release(KEY_ESC);
+    }
   }
-  
-  if(digitalRead(blueKey2)==LOW){
-    Keyboard.print(' ');
-    delay(250);
+  //if isn't necessary if profile 2(3) is last
+  else if(currentProfile==2){
+    digitalWrite(Led2, HIGH);
+    //CS or other profile
   }
-  
-  if(digitalRead(blueKey3)==LOW){
-    Keyboard.press(KEY_ESC);
-    Keyboard.release(KEY_ESC);
-    delay(250);
-  }
-  
- }
 }
